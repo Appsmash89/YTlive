3101e01c636b77cb04fc927f8806216cb98a7f19
Harish Iyer <appsmash89@gmail.com>
2025-11-02 17:42:32 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,7 +4,7 @@
 import { getApp, getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+import { getFirebaseConfig } from './config';
 
 /**
  * Initializes Firebase and returns the app, auth, and firestore instances.
@@ -12,6 +12,7 @@ import { firebaseConfig } from './config';
  * This function ensures that Firebase is initialized only once.
  */
 export function initializeFirebase(): { app: FirebaseApp, auth: Auth, firestore: Firestore } {
+  const firebaseConfig = getFirebaseConfig();
   const apps = getApps();
   const app = apps.length > 0 ? apps[0] : initializeApp(firebaseConfig);
   const auth = getAuth(app);
